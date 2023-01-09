2e331620402a3d3f7886aa97633f35ca3f19faff
whh0503 <122242141+whh0503@users.noreply.github.com>
2023-01-09 21:29:22 +0900

    Create AAAI 2022.py

---
+++ Example/AAAI 2022.py
@@ -0,0 +1,40 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import csv
+from time import sleep
+
+csv_filename = "AAAI 2022.csv"
+csv_open = open(csv_filename, "w+", encoding="utf-8")
+csv_writer = csv.writer(csv_open)
+csv_writer.writerow(("Title","Author"))
+
+
+driver = webdriver.Chrome()
+driver.implicitly_wait(3)
+driver.get('https://aaai-2022.virtualchair.net/papers.html?filter=keywords')
+
+site_body = driver.find_element(By.CSS_SELECTOR, 'body')
+
+# for ii in range(1):
+   
+for i in range(5):
+    site_body.send_keys(Keys.PAGE_DOWN)
+    sleep(1)
+html = driver.page_source
+#driver.quit()
+bs = BeautifulSoup(html, "html.parser")
+total_list = bs.select("body > div.container > div.content > div.cards.row > div > div > div.pp-card-header")
+print(len(total_list))
+
+for i in total_list:
+    title = i.select_one("body > div.container > div.content > div.cards.row > div > div > div.pp-card-header > a").text
+    author = i.select_one("body > div.container > div.content > div.cards.row > div > div > div.pp-card-header > h6").text
+    csv_writer.writerow((title,author))
+sleep(1)
+
+# if ii != 0:
+#     driver.find_element(By.CSS_SELECTOR,'#accepted-papers > nav > ul > li:nth-child(13) > a').click()
+    
+csv_open.close()
